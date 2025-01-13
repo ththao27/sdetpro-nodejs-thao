871cc5b51c0815578cde98f1f1641202ecb7ebdb
thaotran <thao.tran@techxcorp.com>
2025-01-13 16:24:53 +0700

    Update lesson for the first day

---
+++ lessons/day-01/VariableLearning.js
@@ -0,0 +1,25 @@
+// Variable
+// hoisting -> declare function
+// = assignment operand
+// Declaration
+let a = 1;
+
+// RE-declaration is NOT allowed
+// Re-assign is acceptable
+a = 2;
+
+// Constant
+const PI_NUMBER = 3.14;
+
+// Not allowed
+//PI_NUMBER = 3.23;
+console.log(Number.MAX_VALUE);
+console.log(Number.MIN_VALUE);
+
+// Check the type
+console.log(typeof(1.7976931348623157e+308 + 1));
+console.log(0.1 + 0.2);
+
+// Boolean
+let amIright = false; // 
+console.log(typeof amIright);
